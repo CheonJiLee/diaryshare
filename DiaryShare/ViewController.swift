5a753e047ee0964ea831faa2afa42e6f11aaf973
CheonJiLee <cjswlddl@naver.com>
2016-08-16 23:10:24 +0900

    Initial Commit

---
+++ DiaryShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiaryShare
+//
+//  Created by 이천지 on 2016. 8. 16..
+//  Copyright © 2016년 DS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
